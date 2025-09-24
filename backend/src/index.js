b20d0349648f4d512378aece5048ac9b308605e9
raurabryan <raurabryan64@gmail.com>
2025-09-24 11:50:44 -0500

    proyecto back

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+res.send('API backend funcioanando');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>{
+	console.log('Servidor escuchando en el puerto ${PORT}');
+});
