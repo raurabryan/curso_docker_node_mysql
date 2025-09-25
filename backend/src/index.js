d22eec9ea1102eba2f070df3ee43750e73e1bde4
raurabryan <raurabryan64@gmail.com>
2025-09-24 22:28:31 -0500

    Se añade variables de entorno separadas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,18 @@
+require('dotenv').config(); 
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+const jwtSecrecet=process.env.JWT_SECRET;
 
 app.use(express.json());
 
+console.log("clave sercreta ", jwtSecrecet);
+console.log("port ", port);
 app.get('/', (req, res) => {
 res.send('API backend funcioanando');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>{
-	console.log('Servidor escuchando en el puerto ${PORT}');
+
+app.listen(port, () =>{
+	console.log('Servidor escuchando en el puerto: ',port);
 });
